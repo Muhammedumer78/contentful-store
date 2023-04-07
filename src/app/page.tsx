f10943c11560da6a977eb9120f1e6befca6932f3
muhammmed umer <www.muhammadumerpk@gmail.com>
2023-04-07 19:34:32 +0500

    main showcase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,50 @@
+import React from 'react'
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import png from "../../public/img1.webp";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function page() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='bg-neutral-600 grid grid-cols-4 p-5 gap-5 '>
+      <div className='bg-white p-5'>
+        <Image src={png} alt="img1" width={400} height={300} className='border-x-fuchsia-50' />
+      <h1 className="text-2xl font-serif font-bold text-center pt-3 underline decoration-double text-black ">
+      Male Shirt 
+      </h1>
+      <p className='text-md mt-2 font-serif'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
+      
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+      <div className='bg-white p-5'>
+        <Image src={png} alt="img1" width={400} height={300} className='border-x-fuchsia-50' />
+      <h1 className="text-2xl font-serif font-bold text-center pt-3 underline decoration-double text-black ">
+      Male Shirt 
+      </h1>
+      <p className='text-md mt-2 font-serif'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
+      
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='bg-white p-5'>
+        <Image src={png} alt="img1" width={400} height={300} className='border-x-fuchsia-50' />
+      <h1 className="text-2xl font-serif font-bold text-center pt-3 underline decoration-double text-black ">
+      Male Shirt 
+      </h1>
+      <p className='text-md mt-2 font-serif'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
+      
       </div>
-    </main>
+      <div className='bg-white p-5'>
+        <Image src={png} alt="img1" width={400} height={300} className='border-x-fuchsia-50' />
+      <h1 className="text-2xl font-serif font-bold text-center pt-3 underline decoration-double text-black ">
+      Male Shirt 
+      </h1>
+      <p className='text-md mt-2 font-serif'>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      </p>
+      
+      </div>
+    </div>
   )
 }
